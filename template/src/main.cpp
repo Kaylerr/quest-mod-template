53da186bafe8293d426060bcd5b65af121d99c87
Kaylerr <71836631+Kaylerr@users.noreply.github.com>
2021-12-20 21:18:07 +0000

    Update main.cpp
    making ui.

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -33,3 +33,39 @@ extern "C" void load() {
     // Install our hooks (none defined yet)
     getLogger().info("Installed all hooks!");
 }
+
+std::optional<UnityEngine::GameObject*> FindComponentWithText(std::string_view find_text) {
+  // find root components in scene
+  auto scene = UnityEngine::SceneManager::GetActiveScene();
+  auto root_objs = scene->GetRootGameObjects();
+  for (int i = 0; i < root_objs->Length(); i++) {
+    auto text_components = root_objs->values[i]->GetComponentsInChildren<HMUI::CurvedTextMeshPro>();
+    for (int j = 0; j < text_components->Length(); j++) {
+      auto text = to_utf8(csstrtostr(text_components->values[i]->get_text()));
+      if (text == find_text) {
+        return text_components->values[i]->get_gameObject();
+      }
+    }
+  }
+  return std::nullopt;
+
+#include "GlobalNamespace/MainMenuViewController.hpp"
+#include "HMUI/CurvedTextMeshPro.hpp"
+MAKE_HOOK_MATCH(MainMenuViewController_DidActivate, &GlobalNamespace::MainMenuViewController::DidActivate, void,
+  GlobalNamespace::MainMenuViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling
+) {
+  MainMenuViewController_DidActivate(self, firstActivation, addedToHierarchy, screenSystemEnabling);
+  auto solo_button = UnityEngine::GameObject::Find(il2cpp_utils::createcsstr("SoloButton"));
+  auto solo_text = solo_button->FindComponent<HMUI::CurvedTextMeshPro>();
+  solo_text->set_text("The mod worked... right?");
+}
+extern "C" void load() {
+  il2cpp_functions::Init();
+  INSTALL_HOOK(
+    getLogger(),
+    MainMenuViewController_DidActivate
+  );
+}
+
+
+
